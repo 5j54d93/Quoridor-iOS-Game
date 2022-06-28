04688d7a645736deb9870a34fd22fdc845b2e011
Ricky Chuang <songsongyyohya@gmail.com>
2022-06-28 12:45:47 +0800

    Initial Commit

---
+++ Quoridor-iOS-Game/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Quoridor-iOS-Game
+//
+//  Created by 莊智凱 on 2022/6/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Quoridor-iOS-Game/Quoridor_iOS_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Quoridor_iOS_GameApp.swift
+//  Quoridor-iOS-Game
+//
+//  Created by 莊智凱 on 2022/6/28.
+//
+
+import SwiftUI
+
+@main
+struct Quoridor_iOS_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
